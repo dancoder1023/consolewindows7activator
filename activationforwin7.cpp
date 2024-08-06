9782ef7fa7a2137ab0fd62c2dffcd3460f7bcadf
Win AR Tech <94983650+dancoder1023@users.noreply.github.com>
2024-08-06 10:55:17 -0700

    Update activationforwin7.cpp

--- activationforwin7.cpp
+++ activationforwin7.cpp
@@ -10,7 +10,7 @@ int main() {
     SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 
     std::cout << "Press Enter to continue activating..." << std::endl;
-    std::cin.get(); 
+    std::cin.get();
 
     system("runas /user:administrator \"cmd /c slmgr -rearm\"");
 
@@ -18,8 +18,11 @@ int main() {
 
         SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
         std::cout << "Please reboot and it will activate windows.\n" << std::endl;
-        std::cout << "After 30 days you have to reopen this application to activate again." << std::endl;
+        std::cout << "This tool will also make a scheduled task" << std::endl;
         SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+
+        
+        system("schtasks /create /tn \"Windows 7 Activator\" /tr \"%USERPROFILE%\\Downloads\\activationforwin7.exe\" /sc monthly /st 00:00");
     }
     else {
         std::cout << "Error: Command failed." << std::endl;
